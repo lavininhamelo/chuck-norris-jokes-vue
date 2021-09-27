829ef06dde51437bdb7de7d02bad9df0a42d1c4a
Lavinia Melo <lavininhamelo@hotmail.com>
2021-09-27 23:44:47 +0100

    feat: arrow button

---
+++ src/components/Buttons/ButtonArrow.vue
@@ -0,0 +1,102 @@
+<template>
+  <div
+    @mouseenter="hover = true"
+    @mouseleave="hover = false"
+    class="button-arrow"
+    :class="{
+      'button-arrow--outline': outline,
+    }"
+  >
+    <span
+      v-if="type === 'left'"
+      :class="direction"
+      class="button-arrow__icon material-icons"
+    >
+      west
+    </span>
+    <button-base :padding="false" textColor="#cfb995" bgColor="transparent">
+      <slot> </slot>
+    </button-base>
+    <span
+      v-if="type === 'right'"
+      :class="direction"
+      class="button-arrow__icon material-icons"
+    >
+      east
+    </span>
+  </div>
+</template>
+
+<script>
+import ButtonBase from "./Base/ButtonBase.vue";
+export default {
+  components: { ButtonBase },
+  props: {
+    type: {
+      type: String,
+      default: "right",
+    },
+    outline: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      hover: false,
+    };
+  },
+  computed: {
+    direction() {
+      let direction = "";
+      if (this.type !== "right") {
+        direction = "button-arrow__icon--right";
+      } else {
+        direction = "button-arrow__icon--left";
+      }
+
+      if (this.hover) {
+        direction += "-hover";
+      }
+      return direction;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.button-arrow {
+  display: flex;
+  align-items: center;
+  color: $toupe;
+  height: 40px;
+  padding: 10px 16px 10px 16px;
+  border-radius: 2px;
+  border: solid 1px $white-three;
+  background-color: $white-two;
+
+  &--outline {
+    border-radius: 0px;
+    border: none;
+    background-color: transparent;
+  }
+
+  &__icon--left {
+    padding-right: 8px;
+
+    &-hover {
+      padding-left: 8px;
+      padding-right: 0px;
+    }
+  }
+
+  &__icon--right {
+    padding-left: 8px;
+
+    &-hover {
+      padding-left: 0px;
+      padding-right: 8px;
+    }
+  }
+}
+</style>
