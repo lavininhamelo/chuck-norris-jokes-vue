156d57f9f1d6f9e30fa0815b32beeec018a059d2
Lavinia Melo <lavininhamelo@hotmail.com>
2021-09-27 23:14:10 +0100

    feat: base components

---
+++ src/components/Base/BaseButton.vue
@@ -0,0 +1,87 @@
+<template>
+  <button :style="style" v-bind="$attrs" class="button">
+    <span class="material-icons button__icon-left">
+      {{ iconLeft || "" }}
+    </span>
+    <slot></slot>
+    <span class="material-icons button__icon">
+      {{ icon || "" }}
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: false,
+    },
+    iconLeft: {
+      type: String,
+      required: false,
+    },
+    bgColor: {
+      type: String,
+      required: false,
+    },
+    textColor: {
+      type: String,
+      required: false,
+    },
+    borderColor: {
+      type: String,
+      required: false,
+    },
+    padding: {
+      type: [String, Boolean],
+      default: "0px 16px",
+    },
+  },
+  computed: {
+    style() {
+      return {
+        borderColor: this.borderColor,
+        color: this.textColor,
+        background: this.bgColor,
+        padding: this.padding,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 48px;
+  padding: 0px;
+  border-radius: 4px;
+  border: none;
+  font-weight: 600;
+  cursor: pointer;
+  text-transform: uppercase;
+  border-width: 2px;
+  border-style: solid;
+  border-color: transparent;
+  position: relative;
+
+  &__:hover {
+    filter: brightness(0.9);
+  }
+
+  &__:active {
+    filter: brightness(0.85);
+  }
+
+  &____icon {
+    margin-left: 4px;
+  }
+
+  &____icon-left {
+    margin-right: 4px;
+  }
+}
+</style>
---
+++ src/components/Base/BaseInput.vue
@@ -0,0 +1,85 @@
+<template>
+  <div
+    class="input"
+    :class="{
+      'input--focus': focus,
+    }"
+  >
+    <input
+      class="input__element"
+      v-bind="$attrs"
+      type="text"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+      @focus="focus = true"
+      @blur="focus = false"
+    />
+
+    <span v-if="icon" class="material-icons">{{ icon }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: false,
+    },
+    value: {
+      required: false,
+    },
+  },
+  data() {
+    return {
+      focus: true,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.input {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
+  background: transparent;
+  height: 48px;
+  width: 100%;
+  border-radius: 4px;
+  border: 1px solid $white-two;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
+  color: $greyish;
+}
+
+.input__element {
+  &::placeholder {
+    color: $greyish;
+    opacity: 1;
+  }
+
+  &:-ms-input-placeholder {
+    color: $greyish;
+  }
+
+  &::-ms-input-placeholder {
+    color: $greyish;
+  }
+}
+
+.input--focus {
+  background: $white;
+  border: 1px solid #aaa;
+  color: $black;
+}
+
+.input__element {
+  flex: 1;
+  color: $white;
+
+  &:focus {
+    color: $black;
+  }
+}
+</style>
