35fce8c8f4bdba8c97d5412cf400f45b031b4e0a
Lavinia Melo <lavininhamelo@hotmail.com>
2021-09-27 23:57:00 +0100

    fix: auto padding, margin and fonts

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,12 @@
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+#app {
+  font-family: Montserrat, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
