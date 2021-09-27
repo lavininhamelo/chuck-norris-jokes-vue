50ad0439e5f8195faa71bb009bfc416c74b11898
Lavinia Melo <lavininhamelo@hotmail.com>
2021-09-27 23:24:25 +0100

    feat: footer base

--- src/components/General/Footer.vue
+++ src/components/General/Footer.vue
@@ -1,9 +1,42 @@
 <template>
-  <footer>I'm a footer</footer>
+  <footer class="footer">
+    <div class="footer__container">
+      <div class="footer__title">Got jokes? Get paid for submitting!</div>
+    </div>
+  </footer>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 172px;
+  background: url("~@/assets/images/footer.jpg");
+
+  &__container {
+    max-width: 1200px;
+    padding: 0px 16px;
+    width: 100%;
+  }
+
+  &__title {
+    width: 200px;
+    font-weight: 600;
+    text-align: left;
+    line-height: 1.25;
+    color: $white;
+    text-transform: uppercase;
+  }
+
+  &__button {
+    margin-left: -20px;
+  }
+}
+</style>
