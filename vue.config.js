5182e7706c93118c0b2f77decdcc0b3a1ab27759
Lavinia Melo <lavininhamelo@hotmail.com>
2021-09-27 22:40:04 +0100

    feat: config sass

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  chainWebpack(config) {
+    config.entry("app").clear().add("./src/main.js").end();
+    config.resolve.alias.set("@", path.join(__dirname, "./src"));
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        sassOptions: {
+          includePaths: [path.resolve(__dirname, "src")],
+          indentedSyntax: false,
+        },
+        prependData: '@import "@/sass/main.scss";',
+      },
+    },
+  },
+};
