375d62cc323da88474f36d814a91d18244adb8c0
Lavinia Melo <lavininhamelo@hotmail.com>
2021-09-28 18:37:31 +0100

    feat: gh pages

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,3 @@
-const path = require("path");
-
 module.exports = {
   publicPath: "/vue-vehiculum-challenge",
-  chainWebpack(config) {
-    config.entry("app").clear().add("./src/main.js").end();
-    config.resolve.alias.set("@", path.join(__dirname, "./src"));
-  },
-  css: {
-    loaderOptions: {
-      sass: {
-        sassOptions: {
-          includePaths: [path.resolve(__dirname, "src")],
-          indentedSyntax: false,
-        },
-        prependData: '@import "@/sass/main.scss";',
-      },
-    },
-  },
 };
